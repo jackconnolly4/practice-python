1ca45651c6c64845dd1d34b3ac3568f15a44bc8d
Jack <jconnolly444@gmail.com>
2024-01-12 17:33:16 -0600

    delib practice

--- delib-practice.py
+++ delib-practice.py
@@ -1,4 +1,17 @@
 # Write a program that uses a variable to store a number, then prints that number plus 10.
 
-number = 2
-print(number + 10)
+# number = 2
+# print(number + 10)
+
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+# word = "hello"
+# print(word.upper())
+
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 10
+number2 = 25
+print(number1 + number2)
