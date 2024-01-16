135662efd8a59217b72a53e1fbac92dce61fb9e3
Jack <jconnolly444@gmail.com>
2024-01-16 16:42:04 -0600

    page 2 of variable delib practice

---
+++ variable-practice2.py
@@ -0,0 +1,19 @@
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+# first_name = "Jack"
+# last_name = "Connolly"
+
+# print(first_name + "" + last_name)
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# first_name = "Jack"
+# last_name = "Connolly"
+
+# print(f"{first_name} #{last_name}")
+
+# Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+word = input("Enter a word: ")
+if word == "marco":
+    print("polo")
