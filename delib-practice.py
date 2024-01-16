094ff3ee1d75b3f03147d2403167c9859e77a07d
Jack <jconnolly444@gmail.com>
2024-01-15 18:30:12 -0600

    delib practice again

--- delib-practice.py
+++ delib-practice.py
@@ -12,6 +12,22 @@
 
 # Write a program that uses variables to store two numbers, then prints the numbers added together.
 
-number1 = 10
-number2 = 25
-print(number1 + number2)
+# number1 = 10
+# number2 = 25
+# print(number1 + number2)
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+# word = "hello"
+# print(word[::-1])
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+# number = 1000
+# print(number * 10)
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "WEEN"
+word2 = "CITY"
+print(word1.upper() + word2.upper())
