1001c614ad31043062556e61ef2e15d9b40d482c
Jack <jconnolly444@gmail.com>
2024-01-17 17:47:48 -0600

    more conditional db

--- conditional-db1.py
+++ conditional-db1.py
@@ -35,3 +35,26 @@
 # else:
 #     print(0)
 
+# Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+# number = 9001
+
+# if number > 9000:
+#   print(1)
+# else:
+#   print(-1)
+
+
+# Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+  
+number = 15
+
+if number < 10:
+  print(9)
+elif number < 20:
+  print(19)
+elif number < 30:
+  print(29)
+else:
+  print(1)
+
