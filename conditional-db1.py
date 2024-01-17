01769366355e34e6b059b2c5123e4edeae362fd5
Jack <jconnolly444@gmail.com>
2024-01-17 17:42:35 -0600

    conditional practice

--- conditional-db1.py
+++ conditional-db1.py
@@ -1,7 +1,37 @@
 # Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-number = 10
-if number == 10:
-  print(0)
-else:
-  print(-1)
+# number = 10
+# if number == 10:
+#   print(0)
+# else:
+#   print(-1)
+
+  # Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# number =11
+# if number < 10:
+#   print(-1)
+# elif number > 10:
+#   print(1)
+# else:
+#   print(0)
+
+
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+# number1 = 2
+# number2 = 4
+# sum = number1 + number2
+
+# if sum < 10:
+#     print(1)
+# else:
+#     print(0)
+
+# alt: 
+
+# if number1 < 10 and number2 < 10:
+#     print(1)
+# else:
+#     print(0)
+
