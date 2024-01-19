6a677bbfabcb6b05780bf59d88d5fab04473064c
Jack <jconnolly444@gmail.com>
2024-01-18 21:28:00 -0600

    more practice

--- conditional-db2.py
+++ conditional-db2.py
@@ -32,4 +32,11 @@
 # elif age >= 60:
 #     ticket_price = 7
 
-print("Ticket price: $" + str(ticket_price))
+# print("Ticket price: $" + str(ticket_price))
+
+# Write a program to store the type of book (regular, reference, or special collection) and the number of days its overdue. Then calculate the fine amount based on the following conditions:
+
+# If the book is a regular book and overdue by up to 7 days, the fine is $1 per day.
+# If the book is a regular book and overdue by more than 7 days, the fine is $2 per day.
+# If the book is a reference book, there is no fine, regardless of the number of days overdue.
+# If the book is a special collection book, the fine is $5 per day, regardless of the number of days overdue.
