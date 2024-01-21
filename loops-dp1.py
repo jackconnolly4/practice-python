bd6fabf636e1aa81a6967f83adba40890264f452
Jack <jconnolly444@gmail.com>
2024-01-20 18:44:13 -0600

    loops dp

--- loops-dp1.py
+++ loops-dp1.py
@@ -1,6 +1,14 @@
 # Write a while loop to print the numbers 1 through 10.
 
-number = 1
-while number <= 10:
-    print(number)
-    number = number + 1
+# number = 1
+# while number <= 10:
+#     print(number)
+#     number = number + 1
+
+# Write a while loop that prints the word "hello" 5 times.
+
+# number = 1
+
+# while number <= 5:
+#   print("hello")
+#   number += 1
