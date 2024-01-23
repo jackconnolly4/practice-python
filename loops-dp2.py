8b97bd13e986ce2c99cd6862e96ed504c2f1aac9
Jack <jconnolly444@gmail.com>
2024-01-22 18:05:36 -0600

    looped hash into array exercise

--- loops-dp2.py
+++ loops-dp2.py
@@ -25,12 +25,25 @@
 # Start with an array of strings and create a new array with each string upcased.
 # For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-words = ["ween", "primus"]
+# words = ["ween", "primus"]
+# index = 0
+# new_words = []
+
+# while index < 2:
+#   new_words.append(words[index].upper())
+#   index += 1
+
+# print(new_words)
+
+# Start with an array of hashes and create a new array of string values from each hash's :name key.
+# For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+hashes = [{"name": "Jack", "age": 27}, {"name": "Harold", "age": 42}]
+strings = []
 index = 0
-new_words = []
 
 while index < 2:
-  new_words.append(words[index].upper())
+  strings.append(hashes[index]["name"])
   index += 1
 
-print(new_words)
+print(strings)
