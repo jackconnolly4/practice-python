e1f49bd35906517b4fce12a249776838cda9203f
Jack <jconnolly444@gmail.com>
2024-01-24 16:48:19 -0600

    split string loop practice

--- loops-dp3.py
+++ loops-dp3.py
@@ -1,14 +1,29 @@
 # Start with an array of numbers and create a new array with only the numbers less than 20.
 # For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-numbers = [2, 3, 4, 66, 78, 9, 12, 44]
-small_numbers = []
+# numbers = [2, 3, 4, 66, 78, 9, 12, 44]
+# small_numbers = []
+# index = 0
+
+# while index < len(numbers):
+#   number = numbers[index]
+#   if number < 20:
+#     small_numbers.append(number)
+#   index += 1
+
+# print(small_numbers)
+
+# Start with an array of strings and create a new array with only the strings that start with the letter "w".
+# For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+words = ["ween", "ween", "primus", "weezer"]
+w_words = []
 index = 0
 
-while index < len(numbers):
-  number = numbers[index]
-  if number < 20:
-    small_numbers.append(number)
+while index < len(words):
+  letter = words[index][0]
+  if letter == "w":
+    w_words.append(words[index])
   index += 1
 
-print(small_numbers)
+print(w_words)
