f81ae070f89a170010a7c83fde9706233a19b50a
Jack <jconnolly444@gmail.com>
2024-01-26 13:49:09 -0600

    string combination loop

--- loops-dp4.py
+++ loops-dp4.py
@@ -1,12 +1,25 @@
 # Start with an array of numbers and compute the sum of all the numbers.
 # For example, [5, 10, 8, 3] becomes 26.
 
-numbers = [5, 10, 8, 3]
-sum = 0
+# numbers = [5, 10, 8, 3]
+# sum = 0
+# index = 0
+
+# while index < len(numbers):
+#   sum = sum + numbers[index]
+#   index += 1
+
+# print(sum)
+
+# Start with an array of strings and combine them all into a single string.
+# For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+words = ["ween", "boognish","brown", "stallion"]
+string = ""
 index = 0
 
-while index < len(numbers):
-  sum = sum + numbers[index]
+while index < len(words):
+  string = string + words[index]
   index += 1
 
-print(sum)
+print(string)
