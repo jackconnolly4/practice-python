daf5425dd71c552997e815fb7a80bdaa825fc852
Jack <jconnolly444@gmail.com>
2024-01-26 13:21:59 -0600

    more loop practice

--- loops-dp3.py
+++ loops-dp3.py
@@ -77,14 +77,29 @@
 # Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 # For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
-things = [{ "name": "chair", "price": 100 }, { "name": "pencil", "price": 1 }, { "name": "book", "price": 4 }]
-short_name_things = []
+# things = [{ "name": "chair", "price": 100 }, { "name": "pencil", "price": 1 }, { "name": "book", "price": 4 }]
+# short_name_things = []
+# index = 0
+
+# while index < len(things):
+#   thing = things[index]
+#   if len(thing["name"]) < 6:
+#     short_name_things.append(thing)
+#   index += 1
+
+# print(short_name_things)
+
+# Start with an array of numbers and create a new array with only the numbers less than 10.
+# For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+numbers = [100, 4, 5, 6000, 100000, 1, 2]
+new_numbers = []
 index = 0
 
-while index < len(things):
-  thing = things[index]
-  if len(thing["name"]) < 6:
-    short_name_things.append(thing)
+while index < len(numbers):
+  number = numbers[index]
+  if number < 10:
+    new_numbers.append(number)
   index += 1
 
-print(short_name_things)
+print(new_numbers)
