c2e7e2e8ae45f2e3aa82ea09f7062f03e950e090
Jack <jconnolly444@gmail.com>
2024-01-31 15:28:43 -0600

    nested loop combining conditionals

--- loops-dp5.py
+++ loops-dp5.py
@@ -19,16 +19,34 @@
 # Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 # For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
 
-letters1 = ["a", "b", "c"]
-letters2 = ["d", "e", "f", "g"]
-combined_letters = []
-index1 = 0
+# letters1 = ["a", "b", "c"]
+# letters2 = ["d", "e", "f", "g"]
+# combined_letters = []
+# index1 = 0
 
-while index1 < len(letters1):
+# while index1 < len(letters1):
+#   index2 = 0
+#   while index2 < len(letters2):
+#     combined_letters.append(letters1[index1] + letters2[index2])
+#     index2 += 1
+#   index1 += 1
+
+# print(combined_letters)
+
+# Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+# For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+letters = ["a", "b", "c", "d"]
+combined_letters = []
+index = 0
+
+while index < len(letters):
+  letter = letters[index]
   index2 = 0
-  while index2 < len(letters2):
-    combined_letters.append(letters1[index1] + letters2[index2])
+  while index2 < len(letters):
+    if index2 != index:
+      combined_letters.append(letter + letters[index2])
     index2 += 1
-  index1 += 1
+  index += 1
 
 print(combined_letters)
