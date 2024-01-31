4b34d51e2df11d1c92ea23536247bcd8790f3f90
Jack <jconnolly444@gmail.com>
2024-01-30 22:05:59 -0600

    number pairs looped into flattened array exercise

--- loops-dp5.py
+++ loops-dp5.py
@@ -1,16 +1,17 @@
 # Use a nested loop to convert an array of number pairs into a single flattened array.
 # For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
 
-# number_pairs = [[1, 3], [8, 9], [2, 16]]
-# flattened_numbers = []
-# index = 0
+number_pairs = [[1, 3], [8, 9], [2, 16]]
+flattened_numbers = []
+index = 0
 
-# while index < len(number_pairs):
-#   index2 = 0
-#   numbers = number_pairs[index]
-#   while index2 < len(numbers):
-#     flattened_numbers.append(numbers)
-#     index2 += 1
-#   index += 1
+while index < len(number_pairs):
+  index2 = 0
+  numbers = number_pairs[index]
+  while index2 < len(numbers):
+    number = numbers[index2]
+    flattened_numbers.append(number)
+    index2 += 1
+  index += 1
 
-# print(flattened_numbers)
+print(flattened_numbers)
