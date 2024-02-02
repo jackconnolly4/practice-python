69af4b094ed346fd1f861d23705beff570b582ee
Jack <jconnolly444@gmail.com>
2024-02-02 12:51:38 -0600

    end of loop deliberate practices

--- loops-dp5.py
+++ loops-dp5.py
@@ -148,19 +148,48 @@
 # Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 # For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
 
-numbers = [2, 5, 3, 1, 0, 7, 11]
-result = "false"
+# numbers = [2, 5, 3, 1, 0, 7, 11]
+# result = "false"
+# index1 = 0
+
+# while index1 < len(numbers):
+#   number = numbers[index1]
+#   index2 = 0
+#   while index2 < len(numbers):
+#     other_number = numbers[index2]
+#     if number != other_number:
+#       if number + other_number == 10 and result == "false":
+#         result = [number, other_number]
+#     index2 += 1
+#   index1 += 1
+
+# print(result)
+
+# Use a nested loop to convert an array of string arrays into a single string.
+# For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
+
+# nested_words = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
+# combined_word = ""
+# index1 = 0
+
+# while index1 < len(nested_words):
+#   index2 = 0
+#   while index2 < len(nested_words):
+#     combined_word += nested_words[index1][index2]
+#     index2 += 1
+#   index1 += 1
+
+# print(combined_word)
+  
+
+nested_words = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
+combined_word = ""
 index1 = 0
+while index1 < len(nested_words):
+    index2 = 0
+    while index2 < len(nested_words[index1]):
+        combined_word = combined_word + nested_words[index1][index2]
+        index2 = index2 + 1
+    index1 = index1 + 1
 
-while index1 < len(numbers):
-  number = numbers[index1]
-  index2 = 0
-  while index2 < len(numbers):
-    other_number = numbers[index2]
-    if number != other_number:
-      if number + other_number == 10 and result == "false":
-        result = [number, other_number]
-    index2 += 1
-  index1 += 1
-
-print(result)
+print(combined_word)
