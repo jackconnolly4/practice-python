461a960ace97bf26bf23ea02a64e43d09169e8ea
Jack <jconnolly444@gmail.com>
2024-02-05 17:32:19 -0600

    appending words to arrays

--- arrays-hashes-dp1.py
+++ arrays-hashes-dp1.py
@@ -1,1 +1,8 @@
 # Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+bands = ["ween", "primus", "butthole surfers"]
+
+bands.append("weezer")
+bands.append("nirvana")
+
+print(bands)
