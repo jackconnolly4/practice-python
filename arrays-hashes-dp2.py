1a6527aadc19f336097d6a632058bce2ea78c031
Jack <jconnolly444@gmail.com>
2024-02-07 14:25:39 -0600

    printing different hash values on different lines

--- arrays-hashes-dp2.py
+++ arrays-hashes-dp2.py
@@ -17,7 +17,16 @@
 
 # Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
-menu = {"sandwich": 5, "bread": 2, "pizza": 7}
-menu["salad"] = 4
+# menu = {"sandwich": 5, "bread": 2, "pizza": 7}
+# menu["salad"] = 4
 
-print(menu)
+# print(menu)
+
+# Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+book = {"title": "Dune", "author": "Frank Hebert", "pages": 800, "language": "English"}
+
+print(book["title"])
+print(book["author"])
+print(book["pages"])
+print(book["language"])
