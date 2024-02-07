aff9c433b0ae7671e308e5964575a10487315580
Jack <jconnolly444@gmail.com>
2024-02-06 19:26:37 -0600

    printing each item of array on separate line

--- arrays-hashes-dp1.py
+++ arrays-hashes-dp1.py
@@ -9,7 +9,17 @@
 
 # Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-letters = ["a", "r", "w", "p"]
-letters[1] = 34
+# letters = ["a", "r", "w", "p"]
+# letters[1] = 34
 
-print(letters)
+# print(letters)
+
+# Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1, 2, 3, 4, 5]
+index = 0
+
+while index < len(numbers):
+  print(numbers[index])
+  index += 1
+
