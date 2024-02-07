efb09b1ba1068db20238869d1b093cf9a828425c
Jack <jconnolly444@gmail.com>
2024-02-07 14:14:30 -0600

    printing hash attributes on separate lines

--- arrays-hashes-dp2.py
+++ arrays-hashes-dp2.py
@@ -1,1 +1,7 @@
 # Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+person = {"name": "Jack", "last name": "Connolly", "email" : "Jack@mail.com"}
+
+print(person["name"])
+print(person["last name"])
+print(person["email"])
