ce8fef3b93126a1a859aeac12da741b43b6865e9
Jack <jconnolly444@gmail.com>
2024-02-07 14:19:15 -0600

    priting single hash from array of hashes

--- arrays-hashes-dp2.py
+++ arrays-hashes-dp2.py
@@ -1,7 +1,16 @@
 # Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
-person = {"name": "Jack", "last name": "Connolly", "email" : "Jack@mail.com"}
+# person = {"name": "Jack", "last name": "Connolly", "email" : "Jack@mail.com"}
 
-print(person["name"])
-print(person["last name"])
-print(person["email"])
+# print(person["name"])
+# print(person["last name"])
+# print(person["email"])
+
+# Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+people = [
+  { "first_name": "Grace", "last_name": "Hopper" },
+  { "first_name": "Mace", "last_name": "Windu" },
+  { "first_name": "Robert", "last_name": "Chase" }
+]
+print(people[0])
