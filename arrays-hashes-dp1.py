cb0986c46e12ede1b9a77139f8a2a7b57dfaa33f
Jack <jconnolly444@gmail.com>
2024-02-06 19:25:24 -0600

    replacing letters with numbers

--- arrays-hashes-dp1.py
+++ arrays-hashes-dp1.py
@@ -1,8 +1,15 @@
 # Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-bands = ["ween", "primus", "butthole surfers"]
+# bands = ["ween", "primus", "butthole surfers"]
 
-bands.append("weezer")
-bands.append("nirvana")
+# bands.append("weezer")
+# bands.append("nirvana")
 
-print(bands)
+# print(bands)
+
+# Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+letters = ["a", "r", "w", "p"]
+letters[1] = 34
+
+print(letters)
