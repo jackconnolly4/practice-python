98d9ec49fe0d76e3fe06171e7c93392b6d5df28a
Jack <jconnolly444@gmail.com>
2024-02-09 17:30:27 -0600

    converting array of arrays into hash

--- arrays-hashes-dp3.py
+++ arrays-hashes-dp3.py
@@ -1,2 +1,14 @@
 # Convert an array of arrays into a hash.
 # For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+
+pairs = [[1, 3], [8, 9], [2, 16]]
+pairs_hash = {}
+index = 0
+while index < len(pairs):
+  key = pairs[index][0]
+  value = pairs[index][1]
+  pairs_hash[key] = value
+  index += 1
+
+
+print(pairs_hash)
