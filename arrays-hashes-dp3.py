d559c37149d3bb9797a7e1364808cc269b8e557c
Jack <jconnolly444@gmail.com>
2024-02-12 20:07:47 -0600

    converting string to hash with keys

--- arrays-hashes-dp3.py
+++ arrays-hashes-dp3.py
@@ -16,11 +16,25 @@
 # Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 # For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
-items = [{ "id": 1, "color": "blue", "price": 32 }, { "id": 2, "color": "red", "price": 12 }]
-items_hash = {}
-index = 0
-while index < len(items):
-  items_hash[items[index]["id"]] = items[index]
-  index += 1
+# items = [{ "id": 1, "color": "blue", "price": 32 }, { "id": 2, "color": "red", "price": 12 }]
+# items_hash = {}
+# index = 0
+# while index < len(items):
+#   items_hash[items[index]["id"]] = items[index]
+#   index += 1
 
-print(items_hash)
+# print(items_hash)
+
+# Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+
+word = "bookkeeper"
+letter_frequencies = {}
+index = 0
+while index < len(word):
+    letter = word[index]
+    if letter not in letter_frequencies:
+        letter_frequencies[letter] = 0
+    letter_frequencies[letter] += 1
+    index += 1
+
+print(letter_frequencies)
